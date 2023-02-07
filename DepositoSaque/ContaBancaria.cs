a521cf3eb811cf1efefc647d89e744ee20ec6202
Juan Pablo Latreille <juanpablolatreille2015@outlook.com>
2023-02-07 19:22:13 -0300

    Adicionar arquivos de projeto.

---
+++ DepositoSaque/ContaBancaria.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepositoSaque
+{
+    public class ContaBancaria
+    {
+        public int NumeroConta { get; private set; }
+        public string TitularBancario { get; set; }
+        public double ValorInicial { get; set; }
+
+        public ContaBancaria(int numeroconta, string titularbancario, double valorinicial)
+        {
+            NumeroConta = numeroconta;
+
+            TitularBancario = titularbancario;
+
+            ValorInicial = valorinicial;
+        }
+
+        public ContaBancaria()
+        {
+
+        }
+
+        public ContaBancaria(int numeroconta, string titularbancario)
+        {
+            NumeroConta = numeroconta;
+            TitularBancario = titularbancario;
+        }
+
+        public ContaBancaria(double valorinicial)
+        {
+            ValorInicial = valorinicial;
+        }
+
+        public void ValorDeposito(double valorinserido)
+        {
+            ValorInicial = ValorInicial + valorinserido;
+        }
+
+        public void ValorSaque(double valorsaquado)
+        {
+            ValorInicial = ValorInicial - valorsaquado;
+        }
+
+        public override string ToString()
+        {
+            return "Dados da conta: " + NumeroConta + " Titular: " + TitularBancario + ", " + "Saldo: " + ValorInicial.ToString("F2");
+        }
+    }
+}
---
+++ DepositoSaque/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+
+namespace DepositoSaque // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Entre com o numero da conta: ");
+            int numeroconta = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Entre com o nome do titular da conta: ");
+            string titularbancario = Console.ReadLine();
+
+            ContaBancaria contabancaria = new ContaBancaria(numeroconta, titularbancario);
+
+            System.Console.WriteLine("Havera deposito inicial (s/n) ?");
+            char resposta = char.Parse(Console.ReadLine());
+
+            if (resposta == 's' || resposta == 'S')
+            {
+                System.Console.WriteLine("Entre com o deposito inicial: ");
+                double depositoinicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                contabancaria.ValorDeposito(depositoinicial);
+                System.Console.WriteLine(contabancaria);
+            }
+            else
+            {
+                System.Console.WriteLine(contabancaria);
+            }
+
+            System.Console.WriteLine("------------------------------------------------------------");
+
+            System.Console.WriteLine("Entre um valor para deposito:");
+            double valorinserido = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contabancaria.ValorDeposito(valorinserido);
+            System.Console.WriteLine("Dados atualizados:");
+            System.Console.WriteLine();
+            System.Console.WriteLine(contabancaria);
+
+            System.Console.WriteLine("-----------------------------------------------------------");
+
+            System.Console.WriteLine("Entre um valor para saque: ");
+            double valorsaquado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (contabancaria.ValorInicial > valorsaquado)
+            {
+                contabancaria.ValorSaque(valorsaquado);
+                System.Console.WriteLine("Dados atualizados da conta:");
+                System.Console.WriteLine();
+                System.Console.WriteLine(contabancaria);
+            }
+            else
+            {
+                System.Console.WriteLine("-----------------------------------------------------------");
+                System.Console.WriteLine("Saldo insuficiente");
+            }
+        }
+    }
+}
